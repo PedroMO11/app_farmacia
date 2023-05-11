b7a39732e4bd7962fea3b705111e2cf46c9372bb
Aaron Coorahua <ruben.coorahua@utec.edu.pe>
2023-05-11 13:54:24 -0500

    actualización de frontend y carrito de compra(Falta incluir llamado a la actualización de stock)

--- connection.py
+++ connection.py
@@ -1,7 +1,7 @@
 DB_HOST = 'localhost'
 DB_PORT = '5432'
 DB_USER = 'postgres'
-DB_PASSWORD = 'admin'
-DB_SCHEMA = 'farmacia_tarea'
+DB_PASSWORD = '123'
+DB_SCHEMA = 'farmacia'
 
 DB_URI = 'postgresql://{}:{}@{}:{}/{}'.format(DB_USER, DB_PASSWORD,DB_HOST,DB_PORT,DB_SCHEMA)
