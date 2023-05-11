1b6d157425525f8e09ba598359ca323ae89f0262
Aaron Coorahua <ruben.coorahua@utec.edu.pe>
2023-05-11 16:36:22 -0500

    images

--- static/app.js
+++ static/app.js
@@ -179,24 +179,20 @@ function actualizarTotalCarrito(){
     var carritoContenedor = document.getElementsByClassName('carrito')[0];
     var carritoItems = carritoContenedor.getElementsByClassName('carrito-item');
     var total = 0;
-
     //recorremos cada elemento del carrito para actualizar el total
     for(var i=0; i< carritoItems.length;i++){
         var item = carritoItems[i];
         var precioElemento = item.getElementsByClassName('carrito-item-precio')[0];
-        console.log(precioElemento);
-
         //quitamos el simobolo peso y el punto de milesimos.
-        var precio = parseFloat(precioElemento.innerText.replace('S/','').replace('.',''));
+        var precio = parseFloat(precioElemento.innerText.replace('$','').replace('.',''));
         var cantidadItem = item.getElementsByClassName('carrito-item-cantidad')[0];
         console.log(precio);
-        
         var cantidad = cantidadItem.value;
         total = total + (precio * cantidad);
-
     }
     total = Math.round(total * 100)/100;
-    document.getElementsByClassName('carrito-precio-total')[0].innerText = 'S/.'+total.toLocaleString("pe");
+
+    document.getElementsByClassName('carrito-precio-total')[0].innerText = '$'+total.toLocaleString("es") + ",00";
 
 }
 
