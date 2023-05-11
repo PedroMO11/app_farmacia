cd66241f51651aaba2389a68970ceb3b9c3b4794
Aaron Coorahua <ruben.coorahua@utec.edu.pe>
2023-05-11 01:13:12 -0500

    cambios en el model.py

--- models.py
+++ models.py
@@ -13,9 +13,9 @@ class User(db.Model):
 class Product(db.Model):
     __tablename__ = 'products'
     code = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(2))
-    stock = db.Column(db.String(120))
-    price = db.Column(db.String(120))
+    name = db.Column(db.String(200))
+    stock = db.Column(db.Integer)
+    price = db.Column(db.Float)
 
     
     def __repr__(self):
